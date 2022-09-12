d74c15f352389c60d57fce008197eaac30d78bce
BombayV <OMGmsebastian@gmail.com>
2022-09-12 01:58:23 -0400

    tesT

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,11 +5,20 @@ export default defineNuxtConfig({
   mode: 'universal',
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   head: {
-    titleTemplate: (title) => `${title} | Bombay`,
+    title: "Bombay's Blog",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'My personal website including portfolio, FiveM projects, and photography.' }
+      { hid: 'description', name: 'description', content: 'My personal website/blog including portfolio, projects, and photography.' },
+      { hid: 'og:title', property: 'og:title', content: 'Bombay Blog' },
+      { hid: 'og:og:description', property: 'og:description', content: 'My personal website/blog including portfolio, projects, and photography.' },
+      { name: "theme-color", hid: "theme-color", property: 'theme-color', content: '#6366f1' },
+      { hid: 'og:url', property: 'og:url', content: 'https://imbombay.tech' },
+      { hid: 'og:image', property: 'og:image', content: 'https://i.imgur.com/cjlvzoP.png' },
+      { hid: 'og:image:type', property: 'og:image:type', content: 'image/png' },
+      { hid: 'og:image:width', property: 'og:image:width', content: '128' },
+      { hid: 'og:image:height', property: 'og:image:height', content: '128' },
+
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
