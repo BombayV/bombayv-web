d7f01ca8924669da506551edaded06ca9991020b
Bombay <OMGmsebastian@gmail.com>
2022-09-12 12:45:13 -0400

    A

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -32,11 +32,11 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="h-auto bg-zinc-200 dark:bg-zinc-900 duration-150">
+  <div class="h-auto bg-zinc-200 pb-8 dark:bg-zinc-900 duration-150">
 		<CoverImg @close="activeData.url = ''" v-if="activeData.url !== ''" :title="activeData.title" :imgUrl="activeData.url"/>
     <h1 class="text-5xl md:text-6xl pt-24 pb-4 font-mont font-bold border-b-2 border-zinc-500 text-zinc-900 dark:text-zinc-200 text-center mx-auto w-96" :class="activeData.url !== '' && 'blur'">Gallery</h1>
-    <div v-if="pics.length !== 0" class="px-16 py-6 columns-xs gap-6" :class="activeData.url !== '' && 'blur'">
-      <img v-for="image in pics" @click="activeData = { title: image.title, url: image.url }" class="cursor-pointer hover:opacity-90 rounded-md img-shadow border dark:border-zinc-800 mt-6 duration-150" :src="image.url" alt="Loading image...">
+    <div v-if="pics.length !== 0" class="px-16 pt-2 pb-2 columns-xs gap-6" :class="activeData.url !== '' && 'blur'">
+      <img v-for="image in pics" @click="activeData = { title: image.title, url: image.url }" class="cursor-pointer hover:opacity-90 rounded-md img-shadow border dark:border-zinc-800 mt-6 duration-150" :src="image.url" loading="lazy" alt="Loading image...">
     </div>
   </div>
 </template>
@@ -50,5 +50,14 @@ definePageMeta({
       0 8px 8px hsl(0deg 0% 0% / 0.075),
       0 16px 16px hsl(0deg 0% 0% / 0.075)
   ;
+}
+
+img[data-src] {
+	filter: blur(0.2em);
+}
+
+img {
+	filter: blur(0em);
+	transition: filter 0.5s;
 }
 </style>
