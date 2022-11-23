9eb7fbc7276598fcc243ed86189a90030bc23b84
Bombay <OMGmsebastian@gmail.com>
2022-11-23 10:40:45 -0500

    discord auth

---
+++ components/LoginButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {useSupabaseClient} from "#imports";
+
+defineProps({
+  providerType: {
+    type: String,
+    required: true
+  },
+  logo: {
+    type: String,
+    required: false
+  }
+})
+
+const supabase = useSupabaseClient()
+
+const login = async (provider) => {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider
+  })
+}
+</script>
+
+<template>
+  <button @click="login(providerType)" type="button" data-mbd-ripple="true" class="h-8 active:bg-zinc-500 dark:active:bg-zinc-800 grid place-items-center grow bg-zinc-500 hover:bg-zinc-400 dark:hover:bg-zinc-700 shadow-md border-r-2 border-b-2 border-zinc-400 dark:border-0 dark:bg-zinc-800 rounded px-4 transition-colors duration-150">
+    <img class="h-4" :src="logo" alt="Small icon"/>
+  </button>
+</template>
