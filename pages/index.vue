289907e8b6f89341e76ae8b54903f5d6e69993f5
BombayV <OMGmsebastian@gmail.com>
2023-06-08 07:03:27 -0400

    idk

--- pages/index.vue
+++ pages/index.vue
@@ -45,14 +45,14 @@
               <p class="text-lg text-left mt-1">BombayTech was created as a personal website where I could learn more about web technologies. It hosted some projects I had created along with my portfolio and a gallery for my work as an artist. However, after my database got taken down, I decided to learn about Supabase and recreate what once was supposed to be my blog.</p>
             </div>
           </div>
-          <div class="flex flex-col md:flex-row mt-8 md:mt-16">
+          <div class="flex flex-col md:flex-row mt-12">
             <div class="md:mr-8 md:mt-0 mt-6 order-2 md:order-1 flex flex-col">
               <NuxtLink to="https://bombayv.github.io/" target="_blank" class="text-2xl font-bold text-left text-primary-content/90 border-b border-b-transparent w-fit hover:border-b-primary-content/80 transition-colors duration-200">MPhotography</NuxtLink>
               <p class="text-lg text-left mt-1">I wanted to learn about web design, development, and how to host a website. MPhotography was a page created to show all the photos I have taken throughout my career as a photographer. However, due to it being hardcoded, it became very difficult to keep adding more and more images. I soon left it as an abandoned project which remains in the limbo with the last images I uploaded at the time.</p>
             </div>
-            <NuxtImg class="rounded shadow-md drop-shadow-md w-full md:w-1/2 h-full object-cover order-1 md:order-2" src="/mphotography.jpg" alt="Old Bombay Tech"/>
+            <NuxtImg class="rounded shadow-md drop-shadow-md w-full md:w-1/2 h-full object-cover order-1 md:order-2 pl-4 git st" src="/mphotography.jpg" alt="Old Bombay Tech"/>
           </div>
-          <div class="flex flex-col md:flex-row mt-8">
+          <div class="flex flex-col md:flex-row mt-12">
             <NuxtImg class="rounded shadow-md drop-shadow-md w-full md:w-1/2 h-full object-cover" src="/entityevolution.jpg" alt="Old Bombay Tech"/>
             <div class="md:ml-8 md:mt-0 mt-6 flex flex-col">
               <NuxtLink to="https://entityevolution.github.io/" target="_blank" class="text-2xl font-bold text-left text-primary-content/90 border-b border-b-transparent w-fit hover:border-b-primary-content/80 transition-colors duration-200">Entity Evolution</NuxtLink>
