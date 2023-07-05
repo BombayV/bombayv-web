86d0cf99005fae20f469671490aed6090ceb53d5
BombayV <OMGmsebastian@gmail.com>
2023-07-05 00:06:17 -0400

    add wip pages

--- pages/support/privacy.vue
+++ pages/support/privacy.vue
@@ -1,3 +1,14 @@
+<script lang="ts" setup>
+</script>
+
 <template>
-  <div></div>
+  <div>
+    <NuxtLayout name="navbar">
+      <div
+          class="relative max-w-7xl w-full h-screen mx-auto flex flex-col items-center justify-center">
+        <h1 class="font-bold text-8xl text-primary-content/90">WIP</h1>
+        <p class="text-2xl text-primary-content/50">This page is under construction.</p>
+      </div>
+    </NuxtLayout>
+  </div>
 </template>
