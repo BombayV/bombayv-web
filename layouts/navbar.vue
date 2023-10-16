59426d555c93e1d3cfe0ae2c13e02674d49c6127
Bombay <OMGmsebastian@gmail.com>
2023-10-15 21:42:15 -0400

    feat(core): dark mode features

--- layouts/navbar.vue
+++ layouts/navbar.vue
@@ -1,8 +1,32 @@
+<script lang="ts" setup>
+interface Route {
+  name: string
+  path: string
+}
+
+const routes = <Route[]>[
+  { name: 'Apps', path: '/apps' },
+  { name: 'Gallery', path: '/gallery' },
+  { name: 'Contact', path: '/contact' },
+]
+
+const user = useSupabaseUser()
+console.log()
+</script>
+
 <template>
   <div class="w-full flex flex-col">
-    <header>
-      Nav
+    <header class="h-20 bg-red-500 flex items-center justify-between mx-auto max-w-7xl w-full px-6">
+      <div class="font-black text-xl">
+        BombayTech
+      </div>
+      <nav class="hidden md:flex">
+        <NuxtLink v-for="route in routes" :key="route.name" :to="route.path" class="mx-2">
+          {{ route.name }}
+        </NuxtLink>
+      </nav>
     </header>
     <slot/>
+    <Footer/>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase', '@nuxt/image', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/supabase', '@nuxt/image', '@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
   tailwindcss: {
     cssPath: '~/assets/tailwind.css',
     configPath: 'tailwind.config',
@@ -10,5 +11,11 @@ export default defineNuxtConfig({
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
     redirect: false
+  },
+  googleFonts: {
+    display: 'swap',
+    families: {
+      'DM Sans': [400, 500, 600, 700, 800, 900]
+    }
   }
 })
